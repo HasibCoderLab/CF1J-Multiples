3c7161964d6d3f74bf1b11c2ed48658cf88e9579
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-10-04 11:18:00 +0600

    update

--- Multiples.cpp
+++ Multiples.cpp
@@ -6,3 +6,13 @@
 //                Topic    : [Multiples]
 //                File     :Multiples.cpp
 // ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long A, B;
+    cin >> A >> B;
+    string result = B / A ? "Multiples " : "No Multiples";
+       cout<< result;
+    return 0;
+}
