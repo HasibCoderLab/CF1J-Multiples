e4d84fedd6c431f9295226b1382f471f8f21894c
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-10-04 10:31:08 +0600

    add Problem Topic & File rename

---
+++ Multiples.cpp
@@ -0,0 +1,8 @@
+// ================================================
+//                Platform : Codeforces
+//                Contest  : CF1J
+//                Problem  : Multiples
+//                Language : C++
+//                Topic    : [Multiples]
+//                File     :Multiples.cpp
+// ================================================
